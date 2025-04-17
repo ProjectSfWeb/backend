148e0eb349033940b2871b553297f7a28edb4954
Nikita <nikita.mishin.9924@gmail.com>
2025-04-17 13:28:06 +0300

    настроеннй веб сервер

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from database import engine
 import models
@@ -8,5 +9,9 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
-def root():
-    return {"message": "Сервер работает"}
+def read_root():
+    return {"message": "FastAPI is running"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
